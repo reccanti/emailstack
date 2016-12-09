d3bd825018470e12a94eb07a372bb958e4c40560
Ben Wilcox <reccanti@gmail.com>
2016-12-08 22:24:26 -0500

    Inline CSS using Juice
    Uses Juice to inline CSS and write
    to a dist file

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
 const bs = require('browser-sync').create();
 const juice = require('juice');
 
-bs.watch('src/**/*.*').on('change', bs.reload);
+bs.watch('src/**/*.*', function (event, file) {
+    if (event === 'change') {
+        const name = path.basename(file);
+        fs.readFile(file,'utf8', function(err, contents) {
+            const inline = juice(contents);
+            fs.writeFile(path.resolve('.', './dist/' + name), inline, function (err, doc) {
+                if (err) throw err;
+                console.log(inline);
+            });
+        });
+    }
+});
 
 bs.init({
-    server: './src'
+    server: './dist'
 });
