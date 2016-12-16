2c456da9aec6203b34d4b5b67494fc3ddb10f3d7
Ben Wilcox <reccanti@gmail.com>
2016-12-16 01:20:28 -0500

    Access watch from CLI
    The user can now watch files from the command line.
    Refactors browsersync code into a separate function
    that uses the compile() function.

--- compiler.js
+++ compiler.js
@@ -4,35 +4,43 @@ var path = require('path');
 var bs = require('browser-sync').create();
 var juice = require('juice');
 
-// bs.watch('src/**/*.*', function (event, file) {
-//     if (event === 'change') {
-//         var name = path.basename(file);
-//         fs.readFile(file,'utf8', function(err, contents) {
-//             var inline = juice(contents);
-//             fs.writeFile(path.resolve('.', './dist/' + name), inline, function (err, doc) {
-//                 if (err) throw err;
-//                 console.log(inline);
-//             });
-//         });
-//     }
-// });
-
-// bs.init({
-//     server: './dist'
-// });
-
 /**
  * compile HTML and move it to the specified directory
  */
-function compile(input, outputDir) {
+function compile(input, outputDir, cb) {
     var absInput = path.resolve(input);
     var absOutput = path.resolve(outputDir, 'compiled.html');
     fs.readFile(input, 'utf8', function(err, contents) {
         var inline = juice(contents);
         fs.writeFile(absOutput, inline, function(err, doc) {
             if (err) throw err;
+            console.log('compiled!');
+            if (cb) cb(); 
         });
     });
 }
 
+/**
+ * watch for changes to the specified file and serve the 
+ * generated file
+ */
+function watch(input, outputDir) {
+    var absInput = path.resolve(input);
+    var absOutput = path.resolve(outputDir);
+    bs.watch(absInput, function (event, file) {
+        if (event === 'change') {
+            compile(input, outputDir, function() {
+                bs.reload();
+            });
+        }
+    });
+    bs.init({
+        server: {
+            baseDir: absOutput,
+            index: 'compiled.html'
+        }
+    });
+}
+
 module.exports.compile = compile;
+module.exports.watch = watch;
